205fd2bc8c0095c78bf883316a9df0672cf7ac66
User <matan22508@gmail.com>
2025-03-11 17:17:06 +0200

    First version

--- app/src/main/java/com/example/trivia/GameActivity.java
+++ app/src/main/java/com/example/trivia/GameActivity.java
@@ -1,26 +1,42 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btna1,btna2, btna3,btna4;
     private TextView tvQuestion;
     private TextView tvQuestionNumber,tvPoints,tvGameOver;
-    private Collection collection;
+    private Collection2 collection;
     private Question q;
     private int points;
+    String backgroundColor;
+
+    private LinearLayout ll;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        ll = findViewById(R.id.activity_game);
+
+
+        backgroundColor = getIntent().getStringExtra("background_color");
+        setBackgroundColor(backgroundColor);
+
+
 
         tvQuestion = findViewById(R.id.tvQuestion);
         btna1 = findViewById(R.id.btna1);
@@ -39,10 +55,11 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         tvGameOver.setVisibility(View.INVISIBLE);
 
-        collection = new Collection();
+        collection = new Collection2();
         collection.initQuestions();
 
         nextQuestion();
+
     }
 
     private void nextQuestion() {
@@ -108,4 +125,32 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         tvGameOver.setVisibility(View.INVISIBLE);
         this.nextQuestion();
     }
+
+    public void setBackgroundColor(String color) {
+        switch (color) {
+            case "Red": {
+                ll.setBackgroundColor(Color.RED);
+                break;
+            }
+            case "Blue": {
+                ll.setBackgroundColor(Color.BLUE);
+                break;
+            }
+            case "Yellow": {
+                ll.setBackgroundColor(Color.YELLOW);
+                break;
+            }
+            case "Pink": {
+                ll.setBackgroundColor(Color.argb(100, 100, 100, 100));
+                break;
+            }
+            default: {
+                ll.setBackgroundColor(Color.WHITE);
+            }
+        }
+    }
+
+
+    //ניסיון שלי
+
 }
